b76cb1cb56aa66c7248dc896f9a77e32073eb259
Mahmoud Elazab <mahmoud.talaat@outlook.de>
2025-11-08 13:46:00 +0100

    changing def const.

--- inheritance/Student.java
+++ inheritance/Student.java
@@ -20,26 +20,27 @@ public class Student extends BaseEntity {
 
     // Main method
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc1 = new Scanner(System.in);
         Student student = new Student();
 
         // System.out.print("Enter name: ");
         // student.setName(sc.nextLine());
 
         System.out.print("Enter ID: ");
-        student.setId(sc.nextInt());
-        sc.nextLine(); // consume leftover newline
+        student.setId(sc1.nextInt());
+        sc1.nextLine(); // consume leftover newline
 
         System.out.print("Enter address: ");
-        student.setAddress(sc.nextLine());
+        student.setAddress(sc1.nextLine());
 
         System.out.print("Enter grade: ");
-        student.setGrade(sc.nextDouble());
+        student.setGrade(sc1.nextDouble());
 
         System.out.println("\n--- Student Details ---");
         System.out.println("Name: " + student.getName());
         System.out.println("ID: " + student.getId());
         System.out.println("Address: " + student.getAddress());
         System.out.println("Grade: " + student.getGrade());
+        sc1.close();
     }
 }

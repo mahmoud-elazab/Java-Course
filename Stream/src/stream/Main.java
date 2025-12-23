df38913222f1be405f98b3f2963e1702b68a64aa
Mahmoud Elazab <mahmoud.talaat@outlook.de>
2025-12-23 22:44:50 +0100

    first commit

---
+++ Stream/src/stream/Main.java
@@ -0,0 +1,11 @@
+package stream;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+       
+    }
+}
---
+++ Stream/src/stream/Student.java
@@ -0,0 +1,21 @@
+package stream;
+
+public class Student {
+    String name;
+    String department;
+    double grade;
+
+    // Constructor + Getters
+    Student(String name, String department, double grade) {
+        this.name = name;
+        this.department = department;
+        this.grade = grade;
+    }
+
+    public String getName() { return name; }
+    public String getDepartment() { return department; }
+    public double getGrade() { return grade; }
+}
+
+    
+
